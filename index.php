4b20c07871a8e0895e9caeb5abab650a2d3830bd
James Noonan <warped.demon.prince@hotmail.com>
2020-08-27 20:16:09 +0800

    yo
    hi

--- index.php
+++ index.php
@@ -26,6 +26,6 @@
     <title> Save the kids ! </title>
   </head>
   <body>
-    <img src="../img/bestprojectever.jpg" style="width:1000px; height:2000px; display: flex; align-items: center; justify-content: center"></img>
+    <img src="../img/bestprojectever.jpg" style="width:1000px; height:1500px; margin-left:150px;"></img>
   </body>
 </html>
