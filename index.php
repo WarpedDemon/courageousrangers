2bf35f53b6025c09314b44e63e27fd89f42f72f8
James Noonan <warped.demon.prince@hotmail.com>
2020-08-27 20:01:29 +0800

    up you go
    one of us one of us

--- index.php
+++ index.php
@@ -5,15 +5,12 @@
     <meta name="google-site-verification" content="txgKmeRnG--tbzmjs6dsxKpFw-x3RCL2t_jCDioBfDo" />
     <meta name="google-site-verification" content="xkHDb2_zgTzNpDumgrN7J39GOzcAilbnu3ZnP9G_Q8A" />
 
-    <meta name="description" content="Author: J.N. Author, Site Owner: Nicholas Noonan (Nelgara), Category: Commercial Construction, Length: 5 pages">
-    <meta name="keywords" content="HTML, CSS, XML, JavaScript, Php, Nelgara, Nick, Noonan, Commercial, Construction, Services, Peter, Hooks, Katherine, James, Australian, Australia, Perth, Kallaroo, nelgara, australia, contruction services perth,">
+    <meta name="description" content="Author: J.N. Author, Length: 1 pages">
+    <meta name="keywords" content="HTML, CSS, XML, JavaScript, Php, James">
     <meta name="author" content="James Nicholas Noonan">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <meta property="title" content="nelgara pty ltd index construction services australia commericial perth kallaroo nick noonan">
-    <meta property="url" content="https://nelgara.com.au">
     <meta property="type" content="website">
-    <meta property="site_name" content="nelgara">
+    <meta property="site_name" content="mercinna">
 
     <link rel="stylesheet" href="Css/LandingPage.css">
     <link rel="stylesheet" href="Css/Mobile.css">
@@ -29,74 +26,8 @@
     <script type="text/javascript">
       new WOW().init();
     </script>
-    <title> Nelgara Pty Ltd - index</title>
+    <title> Save the kids ! </title>
   </head>
   <body>
-    <?php include("Php/Header.php"); ?>
-    <?php include("Php/Banner.php");
-      $BannerHandlerHelper->CreateNewContent("Commercial Construction - Services");
-    ?>
-    <?php include("Php/MainContent.php");
-        $MainContentHandlerHelper->CreateNewContent("cheese", "<b>What We Do</b>", "
-          <pre>
-            <pre>
-              <h2>Innovative Commercial and Project Management Solutions</h2>
-            </pre>
-
-            <pre>
-              Nelgara Pty Ltd is an Australian commercial management consulting firm
-              that leverages its significant industry experience to develop and apply
-              best practice solutions to a broad mix of service industries including oil and gas,
-              water treatment, power distribution, infrastructure, resources and mining
-              for both private and public institutions.
-            </pre>
-
-            <pre>
-              We can understand our clients’ needs quickly, as we have worked there,
-              and respond with significant and cost-effective improvements to their
-              operational performance whilst supporting their business goals.
-            </pre>
-
-            <pre>
-              We bring in combined local and global knowledge for the benefit of the Australian
-              construction industry.
-            </pre>
-
-            <pre>
-              <b>We put clients first.</b>
-            </pre>
-          </pre>
-          ");
-
-        $MainContentHandlerHelper->CreateNewContent("cheese", "<b>Expertise</b>", "
-          <pre>
-            Nelgara Pty Ltd brings more than 100 years of experience in delivering
-            solutions across the end to end phases of projects both pre and post
-            construction. Nelgara continues to draw on its actual delivery experience
-            integrated with the latest technology solutions to provide a unique
-            firm that delivers sustainable outcomes for an ever-changing world.
-            We are Skilled in Project Leadership, People, Schedule and Cost Management,
-            Sub-Contractor Management, and Stressed Project Recovery
-            (more consultants are joining monthly).
-          </pre>
-          ");
-
-        $MainContentHandlerHelper->CreateNewContent("cheese", "<b>Locations</b>", "
-          <pre>
-            Nelgara Pty Ltd is based in Perth, WA but consultants have both worked on projects
-            and or being responsible at a business Contracts Manager or Commercial Manager level
-            for projects in Queensland, NSW and Victoria.
-            We can also provide solutions through our network of consultant partners in the industry.
-          </pre>
-          ");
-    ?>
-    <span style="height:50px;"> </span>
-    <?php include("Php/Footer.php");
-     ?>
   </body>
-  <script type="text/javascript">
-    document.addEventListener("DOMContentLoaded", function() {
-      PageHandler.InitializeBanner(["img/perthday.png"], ["img/perthnight.png"]);
-    }, false);
-  </script>
 </html>
